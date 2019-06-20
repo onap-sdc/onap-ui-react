021ad76387234255ab34e3aba752b4cb07a2c92f
Akiva Gubbay <akiva.gubbay@amdocs.com>
2019-06-20 16:17:07 +0300

    adding index.js to npm module

---
+++ index.js
@@ -0,0 +1,155 @@
+'use strict';
+
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+exports.TileFooterCell = exports.TileFooter = exports.TileInfoLine = exports.TileInfo = exports.Tile = exports.Tabs = exports.Tab = exports.SVGIcon = exports.RadioGroup = exports.Radio = exports.Portal = exports.PopupMenu = exports.Panel = exports.Notification = exports.ModalTitle = exports.ModalHeader = exports.ModalFooter = exports.ModalBody = exports.Modal = exports.Input = exports.Checklist = exports.Checkbox = exports.Button = exports.Accordion = undefined;
+
+var _Accordion = require('./lib/components/Accordion.js');
+
+var _Accordion2 = _interopRequireDefault(_Accordion);
+
+var _Button = require('./lib/components/Button.js');
+
+var _Button2 = _interopRequireDefault(_Button);
+
+var _Checkbox = require('./lib/components/Checkbox.js');
+
+var _Checkbox2 = _interopRequireDefault(_Checkbox);
+
+var _Checklist = require('./lib/components/Checklist.js');
+
+var _Checklist2 = _interopRequireDefault(_Checklist);
+
+var _Input = require('./lib/components/Input.js');
+
+var _Input2 = _interopRequireDefault(_Input);
+
+var _Modal = require('./lib/components/Modal.js');
+
+var _Modal2 = _interopRequireDefault(_Modal);
+
+var _ModalBody = require('./lib/components/ModalBody.js');
+
+var _ModalBody2 = _interopRequireDefault(_ModalBody);
+
+var _ModalFooter = require('./lib/components/ModalFooter.js');
+
+var _ModalFooter2 = _interopRequireDefault(_ModalFooter);
+
+var _ModalHeader = require('./lib/components/ModalHeader.js');
+
+var _ModalHeader2 = _interopRequireDefault(_ModalHeader);
+
+var _ModalTitle = require('./lib/components/ModalTitle.js');
+
+var _ModalTitle2 = _interopRequireDefault(_ModalTitle);
+
+var _Notification = require('./lib/components/Notification.js');
+
+var _Notification2 = _interopRequireDefault(_Notification);
+
+var _Panel = require('./lib/components/Panel.js');
+
+var _Panel2 = _interopRequireDefault(_Panel);
+
+var _PopupMenu = require('./lib/components/PopupMenu.js');
+
+var _PopupMenu2 = _interopRequireDefault(_PopupMenu);
+
+var _Portal = require('./lib/components/Portal.js');
+
+var _Portal2 = _interopRequireDefault(_Portal);
+
+var _Radio = require('./lib/components/Radio.js');
+
+var _Radio2 = _interopRequireDefault(_Radio);
+
+var _RadioGroup = require('./lib/components/RadioGroup.js');
+
+var _RadioGroup2 = _interopRequireDefault(_RadioGroup);
+
+var _SVGIcon = require('./lib/components/SVGIcon.js');
+
+var _SVGIcon2 = _interopRequireDefault(_SVGIcon);
+
+var _Tab = require('./lib/components/Tab.js');
+
+var _Tab2 = _interopRequireDefault(_Tab);
+
+var _Tabs = require('./lib/components/Tabs.js');
+
+var _Tabs2 = _interopRequireDefault(_Tabs);
+
+var _Tile = require('./lib/components/Tile.js');
+
+var _Tile2 = _interopRequireDefault(_Tile);
+
+var _TileInfo = require('./lib/components/TileInfo.js');
+
+var _TileInfo2 = _interopRequireDefault(_TileInfo);
+
+var _TileInfoLine = require('./lib/components/TileInfoLine.js');
+
+var _TileInfoLine2 = _interopRequireDefault(_TileInfoLine);
+
+var _TileFooter = require('./lib/components/TileFooter.js');
+
+var _TileFooter2 = _interopRequireDefault(_TileFooter);
+
+var _TileFooterCell = require('./lib/components/TileFooterCell.js');
+
+var _TileFooterCell2 = _interopRequireDefault(_TileFooterCell);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+exports.Accordion = _Accordion2.default;
+exports.Button = _Button2.default;
+exports.Checkbox = _Checkbox2.default;
+exports.Checklist = _Checklist2.default;
+exports.Input = _Input2.default;
+exports.Modal = _Modal2.default;
+exports.ModalBody = _ModalBody2.default;
+exports.ModalFooter = _ModalFooter2.default;
+exports.ModalHeader = _ModalHeader2.default;
+exports.ModalTitle = _ModalTitle2.default;
+exports.Notification = _Notification2.default;
+exports.Panel = _Panel2.default;
+exports.PopupMenu = _PopupMenu2.default;
+exports.Portal = _Portal2.default;
+exports.Radio = _Radio2.default;
+exports.RadioGroup = _RadioGroup2.default;
+exports.SVGIcon = _SVGIcon2.default;
+exports.Tab = _Tab2.default;
+exports.Tabs = _Tabs2.default;
+exports.Tile = _Tile2.default;
+exports.TileInfo = _TileInfo2.default;
+exports.TileInfoLine = _TileInfoLine2.default;
+exports.TileFooter = _TileFooter2.default;
+exports.TileFooterCell = _TileFooterCell2.default;
+exports.default = {
+    Accordion: _Accordion2.default,
+    Button: _Button2.default,
+    Checkbox: _Checkbox2.default,
+    Checklist: _Checklist2.default,
+    Input: _Input2.default,
+    Modal: _Modal2.default,
+    ModalBody: _ModalBody2.default,
+    ModalFooter: _ModalFooter2.default,
+    ModalHeader: _ModalHeader2.default,
+    ModalTitle: _ModalTitle2.default,
+    Notification: _Notification2.default,
+    Panel: _Panel2.default,
+    PopupMenu: _PopupMenu2.default,
+    Portal: _Portal2.default,
+    Radio: _Radio2.default,
+    RadioGroup: _RadioGroup2.default,
+    SVGIcon: _SVGIcon2.default,
+    Tab: _Tab2.default,
+    Tabs: _Tabs2.default,
+    Tile: _Tile2.default,
+    TileInfo: _TileInfo2.default,
+    TileInfoLine: _TileInfoLine2.default,
+    TileFooter: _TileFooter2.default,
+    TileFooterCell: _TileFooterCell2.default
+};
